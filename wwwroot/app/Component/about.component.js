a55be6513f45cbc67b37ed326469ead629289bb0
Vladik <sarabiaaa1@gmail.com>
2017-07-24 15:02:13 +0300

    add dragula service

--- wwwroot/app/Component/about.component.js
+++ wwwroot/app/Component/about.component.js
@@ -10,17 +10,26 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var ng2_dragula_1 = require("ng2-dragula");
 var AboutComponent = (function () {
-    function AboutComponent() {
+    function AboutComponent(dragulaService) {
+        var _this = this;
+        this.dragulaService = dragulaService;
         this.items = [];
+        dragulaService.dropModel.subscribe(function (value) {
+            _this.onDropModel(value.slice(1));
+        });
+        dragulaService.removeModel.subscribe(function (value) {
+            _this.onRemoveModel(value.slice(1));
+        });
     }
-    AboutComponent.prototype.ngOnChanges = function (changes) {
-        for (var propName in changes) {
-            var chng = changes[propName];
-            var cur = JSON.stringify(chng.currentValue);
-            var prev = JSON.stringify(chng.previousValue);
-            console.log(propName + ": currentValue = " + cur + ", previousValue = " + prev);
-        }
+    AboutComponent.prototype.onDropModel = function (args) {
+        var el = args[0], target = args[1], source = args[2];
+        // do something else
+    };
+    AboutComponent.prototype.onRemoveModel = function (args) {
+        var el = args[0], source = args[1];
+        // do something else
     };
     AboutComponent.prototype.AddText = function () {
         var elem = new Data();
@@ -50,7 +59,7 @@ AboutComponent = __decorate([
         selector: 'my-about',
         templateUrl: '/partial/aboutComponent',
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [ng2_dragula_1.DragulaService])
 ], AboutComponent);
 exports.AboutComponent = AboutComponent;
 var Data = (function () {
--- wwwroot/app/Component/about.component.ts
+++ wwwroot/app/Component/about.component.ts
@@ -1,19 +1,37 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, ViewChild } from '@angular/core';
+import { DragulaService } from "ng2-dragula";
 
 
 @Component({
     selector: 'my-about',
-    templateUrl: '/partial/aboutComponent',
-    
+    templateUrl: '/partial/aboutComponent', 
 })
 
 export class AboutComponent {
+
+  
    
+
     items: Data[] = []; 
 
-    constructor() {
-       
+    constructor(private dragulaService: DragulaService) {
 
+        dragulaService.dropModel.subscribe((value:any) => {
+            this.onDropModel(value.slice(1));
+        });
+        dragulaService.removeModel.subscribe((value:any) => {
+            this.onRemoveModel(value.slice(1));
+        });
+    }
+
+    private onDropModel(args:any) {
+        let [el, target, source] = args;
+        // do something else
+    }
+
+    private onRemoveModel(args:any) {
+        let [el, source] = args;
+        // do something else
     }
 
     AddText(): void {
@@ -28,6 +46,7 @@ export class AboutComponent {
         elem.field = "";
         elem.type = "photo";
         this.items.push(elem);
+  
     }
 
     AddVideo(): void {
